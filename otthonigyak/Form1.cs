cc33e0d0d48453ada21d57a025fe01229db806ef
Laptika <Laptika@LAPTIKA>
2024-02-28 15:49:42 +0100

    Add project files.

---
+++ otthonigyak/Form1.cs
@@ -0,0 +1,24 @@
+namespace otthonigyak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    SajátGomb sajátGomb = new SajátGomb();
+                    sajátGomb.Left = i * 30;
+                    sajátGomb.Top = j * 30;
+                    Controls.Add(sajátGomb);
+                }
+            }
+        }
+    }
+}
---
+++ otthonigyak/SajátGomb.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otthonigyak
+{
+    public class SajátGomb : Button
+    {
+        int Méret = 30;
+        public static List<string> lista = new List<string>() { "teszt szöveg 1", "teszt szöveg 2" };
+        public SajátGomb()
+        {
+            Text = ":)";
+            Height = Méret;
+            Width = Méret;
+            MouseEnter += SajátGomb_MouseEnter;
+            MouseLeave += SajátGomb_MouseLeave;
+            MouseClick += SajátGomb_MouseClick;
+        }
+
+        private void SajátGomb_MouseClick(object? sender, MouseEventArgs e)
+        {
+            MessageBox.Show(lista[0] +"\n"+ lista[1]);
+        }
+
+        private void SajátGomb_MouseLeave(object? sender, EventArgs e)
+        {
+            BackColor = Color.White;
+        }
+
+        private void SajátGomb_MouseEnter(object? sender, EventArgs e)
+        {
+            BackColor = Color.Fuchsia;
+        }
+    }
+}
